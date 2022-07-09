8a6a0e9a89266ee897c404f0b1182e4031337891
Ishu Bansal <Bansalishu23@gmail.com>
2022-07-09 13:11:02 +0530

    SimpleList

--- src/index.js
+++ src/index.js
@@ -3,46 +3,40 @@ import ReactDom from 'react-dom';
 
 import './index.css'
  
+const books  = [
+   {
+      img : 'https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg',
+      Title: "Ikigai: The Japanese secret to a long and happy life",
+      author: 'Héctor García and Francesc Miralles '
+   },
 
-const firstBook = {
+   {
    
-     img : 'https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg',
-Title: "Ikigai: The Japanese secret to a long and happy life",
- author: 'Héctor García and Francesc Miralles '
-}
-const secondBook = {
-   
-     img : 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL232_SR232,232_.jpg',
+  img : 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL232_SR232,232_.jpg',
 Title: "Atomic Habits: The life-changing",
  author: 'James Clear'
-}
-const thirdBook = {
+   },
+   {
    
      img : 'https://m.media-amazon.com/images/I/41r6F2LRf8L._BG0,0,0,0_FMpng_AC_SY320_SX320_.jpg',
 Title: "The Psychology of Money",
  author: ' Morgan Housel'
-}
+   },
+  ];
+const name = ["ishu","bansal"]
 
+const newName = name.map((name) => {
+
+  return <h1>{name}</h1>
+
+});
 
 
 function Booklist()
 {
   return (
   <section className = "booklist">
-  <Book img ={firstBook.img}
-   Title = {firstBook.Title} 
-   author = {firstBook.author}
-   > <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-     Facere excepturi officiis ducimus sint veritatis 
-     incidunt? Obcaecati facere temporibus nobis repellendus.</p></Book>
-  <Book img ={secondBook.img}
-   Title = {secondBook.Title}
-    author = {secondBook.author}
-    />
-  <Book img ={thirdBook.img} 
-  Title = {thirdBook.Title} 
-  author = {thirdBook.author}
-  />
+ {newName}
   </section>
   );   
 }
